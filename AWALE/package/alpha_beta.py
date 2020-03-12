07b9782f378e5431116fe73d1947812f137971ca
Kenza <Kenza@172.30.137.58>
2020-03-12 18:35:01 +0100

    projects

---
+++ AWALE/package/alpha_beta.py
@@ -0,0 +1,17 @@
+'''
+Created on 12 mars 2020
+
+@author: Kenza
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ AWALE/package/main.py
@@ -0,0 +1,8 @@
+'''
+Created on 12 mars 2020
+
+@author: Kenza
+'''
+
+if __name__ == '__main__':
+    pass
